adcec30a67c732d9984f72d83a46c4355223333e
Peera Aronrat <peelaxv@gmail.com>
2023-09-13 02:51:48 +0700

    first commit

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -1,3 +1,10 @@
+import connectMongoDB from "@/libs/mongodb";
+import { NextResponse } from "next/server";
+
 export async function POST(){
+    const { title, description} = await request.json();
+    await connectMongoDB();
+    await Topic.create({ title, description})
+    return NextResponse.json({ message: "Topic Created "} , {status: 201});
 
 }
